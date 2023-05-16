08af11bd66f549b24037cc601a53cb688a1c19c4
ElenaMityukova <124844232+ElenaMityukova@users.noreply.github.com>
2023-05-16 19:41:53 +0300

    Add files via upload

---
+++ lesson_1_1.6_step_8.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/find_xpath_form")
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Elena")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Drigant")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("SPb")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+
+    button = browser.find_element(By.XPATH, "//button[@type='submit']")
+    button.click()
+
+finally:
+    sleep(30)
+    browser.quit()
+    
