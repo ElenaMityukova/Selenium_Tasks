34af165fb5ac61f3de9fdbe9bf1c1792500693fa
HelenVirtanen <124844232+HelenVirtanen@users.noreply.github.com>
2024-05-18 10:31:01 +0300

    Update lesson1_6_step2.py

--- lesson1_6_step2.py
+++ lesson1_6_step2.py
@@ -1,7 +1,17 @@
+# подключаем webdriver - набор команд для управления браузером
 from selenium import webdriver
+
+# импортируем класс By, который позволяет выбрать способ поиска элемента
 from selenium.webdriver.common.by import By
 
+# инициализируем драйвер браузера, увидим новое открытое окно браузера
 browser = webdriver.Chrome()
+
+# открываем сайт по указанной ссылке
 browser.get("http://suninjuly.github.io/simple_form_find_task.html")
+
+# ищем нужный элемент на сайте по его id
 button = browser.find_element(By.ID, "submit_button")
+
+# помним, что нужно закрыть окно браузера
 browser.quit()
